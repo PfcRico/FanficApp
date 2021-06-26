fe79700e673b7eff8f207613ae4469fab1345ec4
Barys <79482411+PfcRico@users.noreply.github.com>
2021-06-26 20:53:02 +0300

    feature: interceptor added

--- app/src/main/java/com/barys/fanficapp/data/api/FanficDBClient.kt
+++ app/src/main/java/com/barys/fanficapp/data/api/FanficDBClient.kt
@@ -1,20 +1,26 @@
 package com.barys.fanficapp.data.api
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-const val BASE_URL = "http://localhost:9191"
+const val BASE_URL = "http://10.0.2.2:9191/"
 
 object FanficDBClient {
 
     fun getClient(): FanficDBInterface {
 
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
         val okHttpClient = OkHttpClient.Builder()
             .connectTimeout(60,
-            TimeUnit.SECONDS).build()
+            TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
 
         return Retrofit.Builder()
             .client(okHttpClient)
