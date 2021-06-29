59a572e7f2ac259319291cd59e35fd11f4fbad0c
Barys <79482411+PfcRico@users.noreply.github.com>
2021-06-29 20:38:42 +0300

    fix: correct binding process

--- app/src/main/java/com/barys/fanficapp/adapter/FanficPagedListAdapter.kt
+++ app/src/main/java/com/barys/fanficapp/adapter/FanficPagedListAdapter.kt
@@ -2,24 +2,61 @@ package com.barys.fanficapp.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.barys.fanficapp.R
 import com.barys.fanficapp.data.repository.NetworkStatus
 import com.barys.fanficapp.data.vo.Content
 import com.barys.fanficapp.databinding.ActivityMainBinding
 import com.barys.fanficapp.databinding.FanficListItemBinding
 import com.barys.fanficapp.single_fanfic.SingleFanfic
 
-class FanficPagedListAdapter: PagedListAdapter<Content, RecyclerView.ViewHolder>(FanficDiffCallback()) {
+class FanficPagedListAdapter(public val context: Context): PagedListAdapter<Content,
+        RecyclerView.ViewHolder>(FanficDiffCallback()) {
+
+    val FANFIC_VIEW_TYPE = 1
+    val NETWORK_VIEW_TYPE = 2
+
+    private var networkStatus: NetworkStatus? = null
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if (getItemViewType(position) == FANFIC_VIEW_TYPE){
+            (holder as FanficViewHolder).bind(getItem(position), context)
+        } else {
+            (holder as NetworkStatusItemViewHolder).bind(networkStatus)}
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view: View
+
+        if (viewType == FANFIC_VIEW_TYPE){
+            view = layoutInflater.inflate(R.layout.fanfic_list_item, parent, false)
+            return FanficViewHolder(view)
+        } else {
+            view = layoutInflater.inflate(R.layout.network_status, parent, false)
+            return NetworkStatusItemViewHolder(view)
+        }
+    }
+
+    private fun hasExtraRow():Boolean {
+        return networkStatus != null && networkStatus != NetworkStatus.LOADED
+    }
+
+    override fun getItemCount(): Int {
+        return super.getItemCount() + if(hasExtraRow()) 1 else 0
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (hasExtraRow() && position == itemCount - 1){
+            NETWORK_VIEW_TYPE
+        } else {
+            FANFIC_VIEW_TYPE
+        }
     }
 
     class FanficDiffCallback: DiffUtil.ItemCallback<Content>(){
@@ -34,10 +71,17 @@ class FanficPagedListAdapter: PagedListAdapter<Content, RecyclerView.ViewHolder>
 
     class FanficViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        private lateinit var binding: FanficListItemBinding
+
+        val binding = FanficListItemBinding.bind(view)
+
+
         fun bind (content: Content?, context: Context){
+
+            val view = binding.root
+
             binding.fanficTitleCv.text = content?.name
             binding.fanficPublish.text = content?.creationDate
+            binding.fandom.text = content?.fandom
 
             val fanficPoster = content?.picUrl
             com.bumptech.glide.Glide.with(context)
@@ -53,9 +97,12 @@ class FanficPagedListAdapter: PagedListAdapter<Content, RecyclerView.ViewHolder>
         }
 
     class NetworkStatusItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        private lateinit var binding: ActivityMainBinding
+
+
 
         fun bind(networkStatus: NetworkStatus?) {
+            var binding = ActivityMainBinding.bind(this.itemView)
+
             if (networkStatus != null && networkStatus == NetworkStatus.LOADING){
                 binding.progressBarFanfics.visibility = View.VISIBLE
             }
@@ -73,6 +120,24 @@ class FanficPagedListAdapter: PagedListAdapter<Content, RecyclerView.ViewHolder>
             else {
                 binding.txtErrorFanfics.visibility = View.GONE
             }
+        }
+
+    }
+    fun setNetworkState(newNetworkStatus: NetworkStatus){
+        val previousState: NetworkStatus? = this.networkStatus
+        val hadExtraRow = hasExtraRow()
+        this.networkStatus = newNetworkStatus
+        val hasExtraRow = hasExtraRow()
+
+        if (hadExtraRow != hasExtraRow()){
+            if (hadExtraRow){
+                notifyItemRemoved(super.getItemCount())
+            } else {
+                notifyItemInserted(super.getItemCount())
+            }
+        }
+        else if(hasExtraRow && previousState != newNetworkStatus){
+            notifyItemChanged(itemCount - 1)
         }
     }
     }
